09c3e5a40ff471781425ef67e8963b317666dc25
jeonjihyeong <spdlqj7014@naver.com>
2023-03-24 14:40:12 +0900

    mobileProgramming

---
+++ KotlinProject/app/src/main/java/com/example/kotlinbasic/chap05/sec01/NullSafeOperator.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinbasic.chap05.sec01
+
+fun strAllCaps(str: String?){
+    val strCaps: String? = str?.uppercase()
+    println(strCaps)
+}
+
+fun main(){
+    strAllCaps(str:null)
+    strAllCaps(str: "kotlin")
+}
